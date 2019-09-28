102e5f9d6ae4aec911e8ce865fee661bee07dc60
FiscalMonkey <mor16112@byui.edu>
2019-09-28 16:04:54 -0600

    Make homepage w/ about and PHP attempt

---
+++ web/week02/about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <title>CS 313 - About Me</title>
+  </head>
+  <body>
+      <div class="container">
+         <div class="page-header">
+            <h1>Uncle Dave's Website</h1>
+         </div>
+      </div>
+      <div class="container">
+         <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="" id="navbarSupportedContent">
+               <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="index.php">Assignments</a>
+                  </li>
+                  <li class="nav-item active">
+                     <a class="nav-link disabled" href="about.php">About Me</a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+         <div class="jumbotron">
+            <p>My name is Dave Moreno. I'm a Junior at BYU-Idaho and I'm working on a BS in Computer Science. I grew up in Alpine, Utah from the time I was one but I was born in Mexico.</p>
+            <p>My current interests include but are not limited to:</p>
+            <ul>
+               <li>Video Games</li>
+               <li><a href="https://brandonsanderson.com/books/">Books by Brandon Sanderson</a></li>
+               <li>Learning about other languages (Russian in particular)</li>
+               <li>Excersising when I have time</li>
+            </ul>
+            <p>Yeah, I'm pretty lame but I have fun with my wife and two dogs.</p>
+         </div>
+      </div>
+
+      <!-- Using PHP to include the same footer -->
+      <?php include("includes/footer.html"); ?>
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ web/week02/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <title>CS 313 - Assignments</title>
+  </head>
+  <body>
+      <div class="container">
+         <div class="page-header">
+            <h1>Uncle Dave's Website</h1>
+         </div>
+      </div>
+      <div class="container">
+         <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div id="navbarSupportedContent">
+               <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                     <a class="nav-link disabled" href="index.php">Assignments</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.php">About Me</a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+         <div class="jumbotron">
+            <ul>Coming Soon!</ul>
+         </div>
+      </div>
+
+      <!-- Using PHP to include the same footer -->
+      <?php include("includes/footer.html"); ?>
+      
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
