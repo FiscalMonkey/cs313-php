f2b477f05b9a3d5cccf1b1ff89c9b5611feed4a9
FiscalMonkey <mor16112@byui.edu>
2019-10-15 16:20:23 -0600

    Further debugging

--- web/week05/05prove.php
+++ web/week05/05prove.php
@@ -21,11 +21,6 @@ try {
    echo 'Error!: ' . $ex->getMessage();
    die();
 }
-/*
-$stmt = $db->prepare('SELECT DISTINCT year FROM motor_tbl ORDER BY year DESC');
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-*/
 ?>
 
 <!DOCTYPE html>
@@ -41,12 +36,12 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<!-- Optional JavaScript -->
+   <!-- Optional JavaScript -->
    <script>
       var make = $("make");
       var model = $("model");
@@ -60,23 +55,23 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
          motor.disabled = true;
          motor.options[0].selected = true;
          var year = $("year").val();
-         $.post("new_year.php",  year: year },
-         function(data) {
-            $("#makes").html(data);
-         });
-      };
+         $.post("new_year.php", { year: year },
+            function(data) {
+               $("#makes").html(data);
+            });
+      }
 
       function newMake() {
          model.disabled = false;
          model.options[0].selected = true;
          motor.disabled = true;
          motor.options[0].selected = true;
-      };
+      }
 
       function newModel() {
          motor.disabled = false;
          motor.options[0].selected = true;
-      };
+      }
    </script>
 
    <title>CS 313 - 05Prove</title>
@@ -106,7 +101,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
          <form id="car">
             <div class="form-group">
                <label class="h5" for="year">Year</label>
-               <select id="year" class="form-control" onclick="newYear()">
+               <select id="year" class="form-control" onchange="newYear()">
                   <option disabled selected value="">Choose Year</option>
                   <?php foreach ($db->query('SELECT DISTINCT year FROM motor_tbl ORDER BY year DESC') as $row) {
                      echo '<option value="' . $row["year"] . '">' . $row["year"] . '</option>';
