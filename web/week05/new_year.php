82104d712f187b9ad47450500f1f3b305f68fb05
FiscalMonkey <mor16112@byui.edu>
2019-10-15 16:32:19 -0600

    Add connection to database

--- web/week05/new_year.php
+++ web/week05/new_year.php
@@ -1,4 +1,23 @@
 <?php 
+try {
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbOpts = parse_url($dbUrl);
+
+   $dbHost = $dbOpts["host"];
+   $dbPort = $dbOpts["port"];
+   $dbUser = $dbOpts["user"];
+   $dbPassword = $dbOpts["pass"];
+   $dbName = ltrim($dbOpts["path"], '/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $ex) {
+   echo 'Error!: ' . $ex->getMessage();
+   die();
+}
+
 $year = $_POST["year"];
 foreach ($db->query('SELECT DISTINCT b.make, a.make_id FROM motor_tbl AS a WHERE a.year = ' . $year . ' INNER JOIN ORDER BY make') as $row) {
    echo '<option value="' . $row["make_id"] . '">' . $row["make"] . '</option>';
