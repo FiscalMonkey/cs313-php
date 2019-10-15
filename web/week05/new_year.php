cd255c1dde703f4a589750bc7a259f7bef4487c4
FiscalMonkey <mor16112@byui.edu>
2019-10-15 16:56:18 -0600

    another possible fix in select statement

--- web/week05/new_year.php
+++ web/week05/new_year.php
@@ -18,8 +18,8 @@ try {
    die();
 }
 
-$year = $_POST["year"];
-foreach ($db->query('SELECT DISTINCT b.make, a.make_id FROM motor_tbl AS a INNER JOIN make_tbl AS b ON (a.make_id = b.make_id) WHERE a.year = ' . $year) as $row) {
+$year = $_POST['year'];
+foreach ($db->query('SELECT DISTINCT b.make, a.make_id FROM motor_tbl AS a INNER JOIN make_tbl AS b ON (a.make_id = b.make_id) WHERE a.year = ' . $year . '') as $row) {
    echo '<option value="' . $row["make_id"] . '">' . $row["make"] . '</option>';
 }
 ?>
