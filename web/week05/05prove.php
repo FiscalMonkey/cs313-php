6ad30ab252d973a9121cd5b98ee4a7099988e7f6
FiscalMonkey <mor16112@byui.edu>
2019-10-15 16:25:40 -0600

    Change js placement

--- web/week05/05prove.php
+++ web/week05/05prove.php
@@ -41,38 +41,7 @@ try {
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-   <!-- Optional JavaScript -->
-   <script>
-      var make = document.getElementById("make");
-      var model = document.getElementById("model");
-      var motor = document.getElementById("motor");
-
-      function newYear() {
-         make.disabled = false;
-         make.options[0].selected = true;
-         model.disabled = true;
-         model.options[0].selected = true;
-         motor.disabled = true;
-         motor.options[0].selected = true;
-         var year = $("year").val();
-         $.post("new_year.php", { year: year },
-            function(data) {
-               $("#makes").html(data);
-            });
-      }
-
-      function newMake() {
-         model.disabled = false;
-         model.options[0].selected = true;
-         motor.disabled = true;
-         motor.options[0].selected = true;
-      }
-
-      function newModel() {
-         motor.disabled = false;
-         motor.options[0].selected = true;
-      }
-   </script>
+   
 
    <title>CS 313 - 05Prove</title>
 </head>
@@ -129,6 +98,39 @@ try {
 
    <!-- Using PHP to include the same footer -->
    <?php include("../week02/footer.html"); ?>
+   
+   <!-- Optional JavaScript -->
+   <script>
+      var make = document.getElementById("make");
+      var model = document.getElementById("model");
+      var motor = document.getElementById("motor");
+
+      function newYear() {
+         make.disabled = false;
+         make.options[0].selected = true;
+         model.disabled = true;
+         model.options[0].selected = true;
+         motor.disabled = true;
+         motor.options[0].selected = true;
+         var year = $("year").val();
+         $.post("new_year.php", { year: year },
+            function(data) {
+               $("#makes").html(data);
+            });
+      }
+
+      function newMake() {
+         model.disabled = false;
+         model.options[0].selected = true;
+         motor.disabled = true;
+         motor.options[0].selected = true;
+      }
+
+      function newModel() {
+         motor.disabled = false;
+         motor.options[0].selected = true;
+      }
+   </script>
 </body>
 
 </html>
