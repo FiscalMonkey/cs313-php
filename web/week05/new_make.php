184fafbf4fe3dce30561bdf43a4c3da5ba62acef
FiscalMonkey <mor16112@byui.edu>
2019-10-17 16:51:46 -0600

    test echo accross lines

--- web/week05/new_make.php
+++ web/week05/new_make.php
@@ -24,4 +24,6 @@ $make_id = $_POST['make_id'];
 foreach ($db->query('SELECT DISTINCT b.model, a.model_id FROM motor_tbl AS a INNER JOIN model_tbl AS b ON a.model_id = b.model_id WHERE a.year = ' . $year . ' AND a.make_id = ' . $make_id) as $row) {
    echo '<option value="' . $row["model_id"] . '">' . $row["model"] . '</option>';
 }
+echo 'something
+on another line';
 ?>
