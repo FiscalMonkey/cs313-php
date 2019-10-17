0a1c017cbe7d4d255798056f7f11e4d864a14b21
FiscalMonkey <mor16112@byui.edu>
2019-10-17 15:11:38 -0600

    Test different query

--- web/week05/new_make.php
+++ web/week05/new_make.php
@@ -19,7 +19,8 @@ try {
 }
 echo '<option disabled selected value="">Choose Model</option>';
 $_SESSION["make_id"] = $_POST['make_id'];
-foreach ($db->query('SELECT DISTINCT b.model, a.model_id FROM motor_tbl AS a INNER JOIN model_tbl AS b ON (a.model_id = b.model_id) WHERE a.year = ' . $_SESSION["year"] . ' AND a.make_id = ' . $_SESSION["make_id"] . '') as $row) {
+
+foreach ($db->query('SELECT DISTINCT b.model, a.model_id FROM motor_tbl AS a INNER JOIN model_tbl AS b ON (a.model_id = b.model_id) WHERE a.make_id = ' . $_SESSION["make_id"] . '') as $row) {
    echo '<option value="' . $row["model_id"] . '">' . $row["model"] . '</option>';
 }
 ?>
