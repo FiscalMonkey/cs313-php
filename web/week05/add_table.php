d03be75ccb7926bc2ed13f8676124b4448923fb3
FiscalMonkey <mor16112@byui.edu>
2019-10-23 23:00:08 -0600

    Test cdn placement

--- web/week05/add_table.php
+++ web/week05/add_table.php
@@ -25,7 +25,7 @@ $db = get_db();
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-   <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
+   
 
    <title>CS 313 - 05Prove</title>
 </head>
@@ -70,18 +70,18 @@ $db = get_db();
          <form id="car" method="post" onsubmit="return validateForm(this)">
             <div class="form-group">
                <label class="h5" for="year">Year</label>
-               <input class="form-control" id="year" type="number" name="year" tabindex="1" autofocus>
+               <input class="form-control" id="year" type="number" name="year" tabindex="1" autofocus required>
                <div id="year_error" class="invalid-feedback"></div>
                <label class="h5" for="make">Make</label>
-               <input class="form-control" id="make" type="text" name="make" tabindex="2">
+               <input class="form-control" id="make" type="text" name="make" tabindex="2" required>
                <label class="h5" for="model">Model</label>
-               <input class="form-control" id="model" type="text" name="model" tabindex="3">
+               <input class="form-control" id="model" type="text" name="model" tabindex="3" required>
                <label class="h5" for="motor">Engine</label>
-               <input class="form-control" id="motor" type="text" name="motor" tabindex="4">
+               <input class="form-control" id="motor" type="text" name="motor" tabindex="4" required>
                <label class="h5" for="oil">Oil Grade</label>
-               <input class="form-control" id="oil" type="text" name="oil" tabindex="5">
+               <input class="form-control" id="oil" type="text" name="oil" tabindex="5" required>
                <label class="h5" for="cap">Engine Oil Capacity</label>
-               <input class="form-control" id="cap" type="number" name="cap" tabindex="6">
+               <input class="form-control" id="cap" type="number" name="cap" tabindex="6" required>
             </div>
             <input class="btn btn-success btn-lg" type="submit" value="Add Vehicle" name="submit" tabindex="7" />
          </form>
@@ -172,6 +172,7 @@ $db = get_db();
          /[]/.test(value);
       }*/
    </script>
+   <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 </body>
 
 </html>
