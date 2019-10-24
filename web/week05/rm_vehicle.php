7f1ab354341d27ad41e5ef40158d63326a0032ce
FiscalMonkey <mor16112@byui.edu>
2019-10-24 14:55:22 -0600

    check status of query

--- web/week05/rm_vehicle.php
+++ web/week05/rm_vehicle.php
@@ -7,7 +7,7 @@ $motor_id = $_POST["motor"];
 $stmt = $db->prepare('DELETE FROM motor_tbl WHERE motor_id = ' . $motor_id);
 $stmt->execute();
 
-header("Location: remove_table.php");
+#header("Location: remove_table.php");
 
 $message = "Vehicle was deleted.";
 echo "<script type='text/javascript'>alert('$message');</script>";
