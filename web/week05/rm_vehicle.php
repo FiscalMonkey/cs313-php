31a60e9e607281c6ad179b7fde58f9678753a0a9
FiscalMonkey <mor16112@byui.edu>
2019-10-24 15:00:42 -0600

    Change alert placement to before redirection

--- web/week05/rm_vehicle.php
+++ web/week05/rm_vehicle.php
@@ -7,8 +7,8 @@ $motor_id = $_POST["motor"];
 $stmt = $db->prepare('DELETE FROM motor_tbl WHERE motor_id = ' . $motor_id);
 $stmt->execute();
 
-header("Location: remove_table.php");
-
 $message = "Vehicle was deleted.";
 echo "<script type='text/javascript'>alert('$message');</script>";
+
+header("Location: remove_table.php");
 ?>
