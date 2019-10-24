32933e27ae820555cdaab5a05a8b9d9e240c2b2d
FiscalMonkey <mor16112@byui.edu>
2019-10-24 15:41:51 -0600

    Make input class that auto capitalizes

--- web/week05/add_table.php
+++ web/week05/add_table.php
@@ -25,9 +25,13 @@ $db = get_db();
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+   <style>
+      .auto-cap {
+         text-transform: capitalize;
+      }
+      </style>
 
-
-   <title>CS 313 - 05Prove</title>
+   <title>Add Vehicle</title>
 </head>
 
 <body>
@@ -55,17 +59,11 @@ $db = get_db();
             </ul>
          </div>
       </nav>
+      <?php if (isset($_POST["submit"])) {
+
+      } ?>
       <div class="jumbotron">
-         <?php if (isset($_POST["submit"])) {
-            /*create variable as array if not set */
-            if (!isset($_SESSION["cars"])) {
-               $_SESSION["cars"] = array();
-            }
-            /* Search array before adding duplicates */
-            if (array_search($_POST["motor"], $_SESSION["cars"]) == false) {
-               array_push($_SESSION["cars"], $_POST["motor"]);
-            }
-         } ?>
+
          <label class="h3" for="car">Add Vehicle to Database</label>
          <form id="car" method="post" onsubmit="return validateForm(this)">
             <div class="form-row">
@@ -80,11 +78,11 @@ $db = get_db();
                </div>
                <div class="col-md-4 mb-3">
                   <label class="h5" for="make">Make</label>
-                  <input class="form-control" id="make" type="text" name="make" tabindex="2" required>
+                  <input class="form-control auto-cap" id="make" type="text" name="make" tabindex="2" required>
                </div>
                <div class="col-md-4 mb-3">
                   <label class="h5" for="model">Model</label>
-                  <input class="form-control" id="model" type="text" name="model" tabindex="3" required>
+                  <input class="form-control auto-cap" id="model" type="text" name="model" tabindex="3" required>
                </div>
             </div>
             <div class="form-row">
