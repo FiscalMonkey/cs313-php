125effb2f8c1731299f1de228e396e1ea12a747e
FiscalMonkey <mor16112@byui.edu>
2019-10-24 00:10:17 -0600

    Remove test print

--- web/week05/new_motor.php
+++ web/week05/new_motor.php
@@ -3,7 +3,7 @@ require "../dbConnect.php";
 $db = get_db();
 
 $motor_id = $_POST["motor_id"];
-echo 'SELECT mt.oil_cap, g1.grade1, g2.grade2 FROM motor_tbl AS mt INNER JOIN grade1_tbl AS g1 ON mt.grade1_id = g1.grade1_id INNER JOIN grade2_tbl AS g2 ON mt.grade2_id = g2.grade2_id WHERE motor_id = ' . $motor_id;
+
 foreach ($db->query('SELECT mt.oil_cap, g1.grade1, g2.grade2 FROM motor_tbl AS mt INNER JOIN grade1_tbl AS g1 ON mt.grade1_id = g1.grade1_id INNER JOIN grade2_tbl AS g2 ON mt.grade2_id = g2.grade2_id WHERE motor_id = ' . $motor_id) as $row) {
    echo '<br /><p class="h5">Your vehicle takes <strong>' . $row["grade1"] . '-' . $row["grade2"] . '</strong> grade oil and holds approx. <strong>' . $row["oil_cap"] . '</strong> liters.</p>';
 }
