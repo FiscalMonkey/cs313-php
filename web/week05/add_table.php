c0f7fbe3c1ec334cda2f101cff04e0e0acd63ae9
FiscalMonkey <mor16112@byui.edu>
2019-10-25 19:09:49 -0600

    Try prepare statement on single line

--- web/week05/add_table.php
+++ web/week05/add_table.php
@@ -75,24 +75,7 @@ $db = get_db();
          $modelSt->execute(array(':model' => $model, ':make' => $make));
          echo 'Model insert attempted<br>';
          // insert motor into database
-         $motorSt = $db->prepare('INSERT INTO motor_tbl (motor, year, model_id, make_id, grade1_id, grade2_id, oil_cap)
-         VALUES 
-         ( :motor
-         , :year
-         ,(SELECT model_id FROM model_tbl WHERE model = :model )
-         ,(SELECT make_id FROM make_tbl WHERE make = :make )
-         ,(SELECT grade1_id FROM grade1_tbl WHERE grade1 = :grade1 )
-         ,(SELECT grade2_id FROM grade2_tbl WHERE grade2 = :grade2 )
-         , :cap )
-         ON CONFLICT (motor) DO NOTHING');
-
-         echo $motor;
-         echo $year;
-         echo $model;
-         echo $make;
-         echo $g1;
-         echo $g2;
-         echo $cap;
+         $motorSt = $db->prepare('INSERT INTO motor_tbl (motor, year, model_id, make_id, grade1_id, grade2_id, oil_cap) VALUES (:motor, :year, (SELECT model_id FROM model_tbl WHERE model = :model), (SELECT make_id FROM make_tbl WHERE make = :make), (SELECT grade1_id FROM grade1_tbl WHERE grade1 = :grade1), (SELECT grade2_id FROM grade2_tbl WHERE grade2 = :grade2), :cap)');
          echo 'Motor Insert compiled<br>';
 
          $motorSt->execute(array(':motor' => $motor, ':year' => $year, ':model' => $model, ':make' => $make, ':grade1' => $g1, ':grade2' => $g2, ':cap' => $cap));
