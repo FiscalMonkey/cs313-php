62eb5a20c6042c530c8187616f8b5e90e3647650
FiscalMonkey <mor16112@byui.edu>
2019-10-31 16:20:17 -0600

    Second attempt

--- web/week05/login.php
+++ web/week05/login.php
@@ -60,35 +60,44 @@
          <br>
          <div class="form-group">
             <label for="signup">Don't have a login?</label>
-            <button id="signup" class="btn btn-primary form-control" data-togle="modal" data-target="#new_user">Sign Up</button>
+            <button id="signup" class="btn btn-primary" data-toggle="modal" data-target="#new_user">Sign Up</button>
          </div>
       </div>
    </div>
-   <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+   <!-- Modal -->
+   <div class="modal fade" id="new_user" tabindex="-1" role="dialog" aria-labelledby="Sign Up" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+         <div class="modal-content">
+            <form id="sign_up_user">
+               <div class="modal-header">
+                  <h5 class="modal-title" id="modalLabel">Sign Up</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true">&times;</span>
+                  </button>
+               </div>
+               <div class="modal-body">
+                  <div class="form-group">
+                     <label for="username1">Username</label>
+                     <input type="text" class="form-control" id="username1" placeholder="Username" required>
+                  </div>
+                  <div class="form-group">
+                     <label for="password1">Password</label>
+                     <input type="password" class="form-control" id="password1" onchange="validPassword()" placeholder="Password" required>
+                  </div>
+                  <div class="form-group">
+                     <label for="password2">Confirm Password</label>
+                     <input type="password" class="form-control" id="password2" onchange="validPassword()" placeholder="Confirm Password" required>
+                     <div class="invalid-feedback" id="password_invalid"></div>
+                  </div>
+               </div>
+               <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                  <button type="button" class="btn btn-primary">Sign Up</button>
+               </div>
+            </form>
+         </div>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+   </div>
 
    <?php include "../week02/footer.html" ?>
 </body>
